74daf8dd18436830178cc61c05fade32d4bf8fc5
sehajmakkar <codewithsehaj@gmail.com>
2025-09-14 17:37:02 +0530

    init backend

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import type { Application, Request, Response } from "express";
+
+import "dotenv/config";
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
