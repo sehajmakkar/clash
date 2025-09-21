549981983aa06c8852d17002212656546d2a1df1
sehajmakkar <codewithsehaj@gmail.com>
2025-09-22 02:51:58 +0530

    chore: viewing engine setup (ejs)

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// Setting View Engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.render("welcome");
 });
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,25 @@ import express from "express";
 import type { Application, Request, Response } from "express";
 
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Setting View Engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!');
+    res.render("welcome");
 });
 
 app.listen(PORT, () => {
